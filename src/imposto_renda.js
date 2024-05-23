1382a1c4d924efb7dfac80b569562d6570a5c755
Hiédi Ferreira <hiedyferreira@gmail.com>
2024-05-23 14:30:15 -0300

    feat: calculo do imposto de renda

---
+++ src/imposto_renda.js
@@ -0,0 +1,28 @@
+/*
+    Implemente uma função que retorne o valor a ser pago de imposto de renda, com base no salário bruto recebido. Use as regras abaixo:
+    Salário até 2.212,00: isento;
+    Salário entre R$ 2.212,01 e R$ 2.826,65: imposto = 7.5% do salário bruto;
+    Salário entre R$ 2.826,66 e R$ 3.751,05: imposto = 15%;
+    Salário entre R$ 3.751,06 e R$ 4.664,68: imposto = 22.5%;
+    Salário acima de R$ 4.664,68: imposto = 27.5%.
+*/
+
+let impostoRenda = 0
+
+function calcularImpostoRenda(salarioBruto) {
+    if(salarioBruto <= 2212){
+        impostoRenda = 0
+    } else if(salarioBruto <= 2826.65){
+        impostoRenda = salarioBruto * 0.075
+    } else if(salarioBruto <= 3751.05){
+        impostoRenda = salarioBruto * 0.15
+    } else if(salarioBruto <= 4664.68){
+        impostoRenda = salarioBruto * 0.225
+    } else {
+        impostoRenda = salarioBruto * 0.275
+    }
+
+    return impostoRenda.toFixed(2)
+}
+
+module.exports = calcularImpostoRenda
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const calcularInss = require("./calculo_inss")
+const calcularImpostoRenda = require("./imposto_renda")
 
-console.log(`O valor da contribuição do INSS referente ao seu salário bruto é de R$${calcularInss(4512)}`)
+let salarioBruto = 8000
+
+console.log(`Salário bruto: R$${salarioBruto} `)
+
+console.log(`O valor da contribuição do INSS referente ao seu salário bruto é de R$${calcularInss(salarioBruto)}`)
+console.log(`O valor do imposto de renda referente ao seu salário bruto é de R$${calcularImpostoRenda(salarioBruto)}`)
